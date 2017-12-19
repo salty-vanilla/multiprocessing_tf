d5af02a3aa7fd9cde8f196f7ef2cdabb0b358693
salty-vanilla <jacketgardensio@gmail.com>
2017-12-19 14:01:00 +0900

    Update train.py

--- train.py
+++ train.py
@@ -5,6 +5,9 @@ from data_generator import DataGenerator
 from model import MLP
 
 
+NB_PROCESS = 10
+
+
 def operation(coord, nb_epoch=10, model_dir='./model'):
     model = MLP()
     data_gen = DataGenerator()
@@ -12,11 +15,15 @@ def operation(coord, nb_epoch=10, model_dir='./model'):
 
 
 def main():
+    # create coordinator for managing multi-thread process
     coord = tf.train.Coordinator()
+    # create multi-thread process
     threads = [threading.Thread(target=operation, args=(coord, 30, 'model_{}'.format(i)))
-               for i in range(10)]
+               for i in range(NB_PROCESS)]
 
+    # process run
     for t in threads:
+        # sleep for process from conflicting
         time.sleep(1)
         t.start()
 
